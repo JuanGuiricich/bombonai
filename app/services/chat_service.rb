b7b6f77b39f9fea96c5063f234ece59e5c3ee497
Juan Guiricich <jguiricich@gmail.com>
2024-02-22 19:27:50 +0100

    add API response handling and base chatting feature with turbo stream

---
+++ app/services/chat_service.rb
@@ -0,0 +1,33 @@
+require 'httparty'
+class ChatService
+
+  include HTTParty
+  base_uri 'http://35.204.143.204:5000'
+
+  def self.send_message(payload)
+    post('/v1/chat/completions', body: payload.to_json, headers: { 'Content-Type' => 'application/json' })
+  end
+end
+
+
+# payload = {
+#   your_name: "Test User",
+#   user_input: "Hello, just testing!",
+#   name1: "Test User",
+#   name2: "AI",
+#   greeting: "Hello, what can I do for you?",
+#   context: "Testing the chat service.",
+#   character: "Example",
+#   messages: [
+#     {
+#       role: "user",
+#       content: "Hello, just testing!"
+#     },
+#     {
+#       role: "assistant",
+#       content: "Hello, what can I do for you?"
+#     }
+#   ]
+# }
+
+# response = ChatService.send_message(payload)
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_22_074644) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_22_173750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "user_name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +31,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_22_074644) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "messages", "users"
 end
