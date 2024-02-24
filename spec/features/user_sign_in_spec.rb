59ba7f0abab55552a6882b008fcae1b07440efb1
Juan Guiricich <jguiricich@gmail.com>
2024-02-24 18:46:10 +0100

    add tests to users and jobs

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "User Sign Ins", type: :feature do
-  let!(:user) { User.create(email: 'user@example.com', password: 'password123') }
+  describe 'User Sign Ins' do
+    let!(:user) { User.create!(username: 'this_is_a_test', email: 'test@gmail.com', password: 'password', password_confirmation: 'password') }
 
-  scenario 'user signs in successfully' do
-    visit new_user_session_path
-
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password123'
-    click_button 'Log in'
-
-    expect(page).to have_content('Signed in successfully.')
+    it 'user signs in successfully' do
+      visit new_user_session_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+      expect(page).to have_content('Signed in successfully.')
+    end
   end
 end
