958d65e36693a499902053078be30e2317054e74
Juan Guiricich <jguiricich@gmail.com>
2024-02-24 15:55:13 +0100

    add feature server table and load balance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_23_083528) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_24_134417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_23_083528) do
     t.bigint "user_id", null: false
     t.boolean "is_user_message"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "servers", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
