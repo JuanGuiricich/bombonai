634ad8f897134d83fd98831c2df1cff8e8b9fe90
Juan Guiricich <jguiricich@gmail.com>
2024-02-25 23:23:27 +0100

    add fix for chat history

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -2,22 +2,29 @@ class SendMessageJob < ApplicationJob
   queue_as :default
 
   def perform(user_id, message_content)
-    api_request = {
-      your_name: User.find(user_id).username,
-      user_input: message_content,
-      name1: User.find(user_id).username,
-      name2: "Tamara",
-      context: "You are Tamara an Argentinian girl that is fan of Boca Juniors and loves to party hard.",
-      character: "Example",
-      messages: [
-        {
-          role: "user",
-          content: message_content
-        }
-      ]
-    }
+  user = User.find(user_id)
+  message_history = user.messages.order(created_at: :asc)
 
-    response = ChatService.send_message(api_request)
+  messages_conversation = message_history.map do |message|
+    {
+      role: message.is_user_message ? "user" : "ai",
+      content: message.content
+    }
+  end
+
+  messages_conversation << { role: "user", content: message_content }
+
+  api_request = {
+    your_name: user.username,
+    user_input: message_content,
+    name1: user.username,
+    name2: "Tamara",
+    context: "You are Tamara an Argentinian girl that is fan of Boca Juniors and loves to party hard.",
+    character: "Example",
+    messages: messages_conversation
+  }
+
+  response = ChatService.send_message(api_request)
 
     if response.success?
       parsed_response = JSON.parse(response.body)
